2e460551293ac156ba8c187d368202d16c526f5c
Sean <a1668979@student.adelaide.edu.au>
2017-07-04 22:09:04 +0930

    Adding the Constructed Evaluator

---
+++ include/ConstructedEvaluator.hpp
@@ -0,0 +1,14 @@
+#ifndef __DECK_CURATOR__CONSTRUCTED_EVALUATOR_H
+#define __DECK_CURATOR__CONSTRUCTED_EVALUATOR_H
+
+#include "Evaluator.hpp"
+#include "Deck.hpp"
+
+namespace DeckCurator {
+    class ConstructedEvaluator : public Evaluator {
+        public:
+            ConstructedEvaluator();
+    };
+};
+
+#endif
